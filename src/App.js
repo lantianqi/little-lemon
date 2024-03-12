fdd85a7d9568ad19a3ed47e5c6e61a0c2f119e5c
lantianqi <50139994+lantianqi@users.noreply.github.com>
2024-03-12 22:22:31 +1100

    add routes to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import Header from "./Header";
 import Nav from "./Nav";
 import Footer from "./Footer";
-import Hero from "./Hero";
+import CTA from "./CTA";
+import Home from "./Home";
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Nav /> */}
-      <Hero />
-      <main>main</main>
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={<CTA />}></Route>
+        <Route></Route>
+        <Route></Route>
+
+      </Routes>
+
       <Footer />
+
     </>
     // <div className="App">
     // </div>
---
+++ src/CTA.js
@@ -0,0 +1,14 @@
+import "./CTA.css";
+
+function Hero() {
+  return (
+    <div className="heroContainer">
+      <div id="CTATitles">
+        <h1 className="title">Little Lemon</h1>
+        <h2 className="subTitle">Chicago</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
