6436d1d5a625d53695c693350f6ef333d40c328e
lantianqi <50139994+lantianqi@users.noreply.github.com>
2024-04-09 16:46:01 +1000

    update web icon

--- src/Footer.js
+++ src/Footer.js
@@ -11,12 +11,36 @@ function Footer() {
       <div id="footerDoormatNav" className="footerColumn">
         <div className="footerColumnTitle">Doormat Navigation</div>
         <ul>
-          <li><Link to="/" className="nav-item">Home</Link></li>
-          <li><Link to="/about" className="nav-item">About</Link></li>
-          <li><Link to="/menu" className="nav-item">Menu</Link></li>
-          <li><Link to="/reservations" className="nav-item">Reservations</Link></li>
-          <li><Link to="/online-order" className="nav-item">Order Online</Link></li>
-          <li><Link to="/login" className="nav-item">Login</Link></li>
+          <li>
+            <Link to="/" className="nav-item">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="nav-item">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/menu" className="nav-item">
+              Menu
+            </Link>
+          </li>
+          <li>
+            <Link to="/reservations" className="nav-item">
+              Reservations
+            </Link>
+          </li>
+          <li>
+            <Link to="/online-order" className="nav-item">
+              Order Online
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" className="nav-item">
+              Login
+            </Link>
+          </li>
         </ul>
       </div>
       <div id="footerContact" className="footerColumn">
